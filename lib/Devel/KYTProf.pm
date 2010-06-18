178d3b41c582f7fa070773f191134ff670619557
onishi <onishi@hatena.ne.jp>
2010-06-18 21:11:34 +0900

    Module::Setup し直した

--- lib/Devel/KYTProf.pm
+++ lib/Devel/KYTProf.pm
@@ -102,3 +102,30 @@ sub add_prof {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Devel::KYTProf -
+
+=head1 SYNOPSIS
+
+  use Devel::KYTProf;
+
+=head1 DESCRIPTION
+
+Devel::KYTProf is
+
+=head1 AUTHOR
+
+Yasuhiro Onishi E<lt>yasuhiro.onishi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
