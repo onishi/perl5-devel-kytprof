96b5f1228015a01ae055c22815044a13f47c8ca7
onishi <yasuhiro.onishi@gmail.com>
2013-02-20 11:01:49 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index de59357..3866153 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,10 @@
     Revision history for Perl extension Devel::KYTProf
    
    +0.04    Tue Feb 19 19:07:11 JST 2013
    +        - Changed label "time" to "runtime" (Wakaba)
    +
     0.03    Tue Feb 19 17:50:20 JST 2013
    -        - Added LTSV logger (wakaba)
    +        - Added LTSV logger (Wakaba)
    
     0.02    Tue Jun 5 16:50:10 JST 2012
             - Added furl support (Songmu)

--- lib/Devel/KYTProf.pm
+++ lib/Devel/KYTProf.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use base qw/Class::Data::Inheritable/;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 __PACKAGE__->mk_classdata( namespace_regex       => undef );
 __PACKAGE__->mk_classdata( ignore_class_regex    => undef );
