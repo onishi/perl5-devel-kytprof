d95db86aa62ff9767f697108b48626c8f8bde261
Songmu <y.songmu@gmail.com>
2019-03-26 12:26:32 +0900

    adjust testing around mysql

---
+++ author/travis_install_mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+sudo service mysql stop
+sudo apt-get install python-software-properties
+cat <<EOC | sudo debconf-set-selections
+mysql-apt-config mysql-apt-config/select-server select  mysql-5.7
+mysql-apt-config mysql-apt-config/repo-distro   select  ubuntu
+EOC
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+sudo dpkg --install mysql-apt-config_0.8.10-1_all.deb
+sudo apt-get update -q
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+sudo mysql_upgrade
