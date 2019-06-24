a21a6e53ae5e690ef325d1bc2ec09161e94072c5
papix <mail@papix.net>
2019-06-24 18:20:53 +0900

    When url or method is undefined, profiler outputs warning message

--- lib/Devel/KYTProf/Profiler/Furl/HTTP.pm
+++ lib/Devel/KYTProf/Profiler/Furl/HTTP.pm
@@ -8,13 +8,32 @@ sub apply {
         'Furl::HTTP',
         'request',
         sub {
-            my($orig, $self, %args) = @_;
+            my ($orig, $self, %args) = @_;
+
+            my $method = $args{method} || 'GET';
+            my $url = do {
+                if (defined $args{url}) {
+                    $args{url};
+                } else {
+                    my $scheme = $args{scheme} || 'http';
+                    my $port = $args{port} || '';
+                    my $host = $args{host} || '';
+                    my $path_query = $args{path_query} || '';
+                    sprintf('%s://%s%s%s',
+                        $scheme,
+                        $host,
+                        $port ? ":$port" : '',
+                        substr($path_query, 0, 1) eq '/' ? $path_query : "/$path_query",
+                    );
+                }
+            };
+
             return [
                 '%s %s',
                 ['http_method', 'http_url'],
                 {
-                    http_method => $args{method},
-                    http_url => $args{url},
+                    http_method => $method,
+                    http_url    => $url,
                 },
             ];
         },
